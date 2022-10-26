87d13b81613a3c6516cac2c534421b17f0e73d38
Bilguun0118 <bi7guunt@gmail.com>
2022-10-26 09:47:27 +0800

    client and server

--- client/src/pages/logInPage.js
+++ client/src/pages/logInPage.js
@@ -1,13 +1,28 @@
-
-
+import axios from "axios";
 const LogIn = () => {
+    const req = async () => {
+        const result = await axios({
+            url: "http://localhost:8000/api/v1/login",
+            method: "POST",
+            headers: {
+                //'Authorization': `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            data: {
+                email: "bilguun@gmail.com",
+                password: "1234"
+            },
+        });
+        console.log(result);
+        return result
+    }   
     return ( 
         <div className="container">
             <h1>Log in to your MyLinkTree</h1>
             <input type="text" placeholder="mail " />
             <input type="text" placeholder="pass" />
             <p>log in with phone number</p>
-            <button>Log in</button>
+            <button onClick={req}>Log in</button>
             <p>forget pass</p>
             <h3>create accaunt</h3>
 
